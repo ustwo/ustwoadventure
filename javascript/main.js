11027ab870f76183e20fa897ee7652e072fc5a82
Neef Rehman <neef.rehman@gmail.com>
2018-02-08 15:27:32 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -290,7 +290,7 @@
   $(".button").click(function(){
     $(".button").not(this).removeClass('is_checked');
     $(this).addClass("is_checked");
-    history.replaceState( {} , 'ustwo Adventure', `/` );
+    history.replaceState( {} , 'ustwo Adventure', '/' );
   });
 
 
