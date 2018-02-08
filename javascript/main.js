7aae9e340a1d9a7e8066c6f0d0bfa78930639623
Neef Rehman <neef.rehman@gmail.com>
2018-02-08 15:50:43 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -319,7 +319,6 @@
 
 
   $(document).ready(function () {
-    var hash = window.location.hash;
     if (window.location.search.indexOf('ref=') > -1) {
       history.replaceState( {} , 'ustwo Adventure', '/' );
     }
