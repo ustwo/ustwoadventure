f2271f23d4178c10c8324222aa617d989ae103bc
Neef Rehman <neef.rehman@gmail.com>
2018-02-08 14:53:33 +0000

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -236,7 +236,7 @@
 
     var j, x, i;
     for (i = a.length; i; i--) {
-      j = Math.playground(Math.random() * i);
+      j = Math.floor(Math.random() * i);
       x = a[i - 1];
       a[i - 1] = a[j];
       a[j] = x;
