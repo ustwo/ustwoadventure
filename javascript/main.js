ae42183875f41c8c538070959ead8496a663deab
Neef Rehman <neef.rehman@gmail.com>
2018-04-20 11:27:51 +0100

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -4,262 +4,522 @@
     {
       "name": "Tribe",
       "url": "https://wearetribe.co/",
-      "tags": "playground fund"
+      "tidyurl": "wearetribe.co",
+      "tags": "playground fund",
+      "image": "images/team_assets/tribe_image.jpg",
+      "logo": "images/team_assets/tribe_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Combo",
       "url": "https://www.combostudio.co",
-      "tags": "playground"
+      "tidyurl": "combostudio.co",
+      "tags": "playground",
+      "image": "images/team_assets/combo_image.jpg",
+      "logo": "images/team_assets/combo_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "23 Code Street",
       "url": "http://www.23codestreet.com",
-      "tags": ""
+      "tidyurl": "23codestreet.com",
+      "tags": "",
+      "image": "images/team_assets/23codestreet_image.jpg",
+      "logo": "images/team_assets/23codestreet_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "The Art of Ping Pong",
       "url": "http://www.theartofpingpong.co.uk/",
-      "tags": "playground"
+      "tidyurl": "theartofpingpong.co.uk",
+      "tags": "playground",
+      "image": "images/team_assets/theartofpingpong_image.jpg",
+      "logo": "images/team_assets/theartofpingpong_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Comuzi",
       "url": "http://www.comuzi.xyz",
-      "tags": "playground"
+      "tidyurl": "comuzi.xyz",
+      "tags": "playground",
+      "image": "images/team_assets/comuzi_image.jpg",
+      "logo": "images/team_assets/comuzi_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Dubzoo",
       "url": "http://www.dubzoo.com",
-      "tags": "playground"
+      "tidyurl": "dubzoo.com",
+      "tags": "playground",
+      "image": "images/team_assets/dubzoo_image.jpg",
+      "logo": "images/team_assets/dubzoo_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Ayla",
       "url": "http://www.aylafurniture.com/",
-      "tags": "playground"
+      "tidyurl": "aylafurniture.com",
+      "tags": "playground",
+      "image": "images/team_assets/ayla_image.jpg",
+      "logo": "images/team_assets/ayla_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Easle",
       "url": "https://www.easle.co",
-      "tags": "playground fund"
+      "tidyurl": "easle.co",
+      "tags": "playground fund",
+      "image": "images/team_assets/easle_image.jpg",
+      "logo": "images/team_assets/easle_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Immersive Rehab",
       "url": "https://www.immersiverehab.com",
-      "tags": ""
+      "tidyurl": "immersiverehab.com",
+      "tags": "",
+      "image": "images/team_assets/immersiverehab_image.jpg",
+      "logo": "images/team_assets/immersiverehab_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Parasym Health",
       "url": "http://www.parasym.co",
-      "tags": "playground"
+      "tidyurl": "parasym.co",
+      "tags": "playground",
+      "image": "images/team_assets/parasymhealth_image.jpg",
+      "logo": "images/team_assets/parasymhealth_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Sanctus",
       "url": "http://sanctus.io",
-      "tags": "playground fund"
+      "tidyurl": "sanctus.io",
+      "tags": "playground fund",
+      "image": "images/team_assets/sanctus_image.jpg",
+      "logo": "images/team_assets/sanctus_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Sidestory",
       "url": "https://sidestory.co",
-      "tags": "playground fund"
+      "tidyurl": "sidestory.co",
+      "tags": "playground fund",
+      "image": "images/team_assets/sidestory_image.jpg",
+      "logo": "images/team_assets/sidestory_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Everyday",
       "url": "http://www.everyday.studio",
-      "tags": "playground"
+      "tidyurl": "everyday.studio",
+      "tags": "playground",
+      "image": "images/team_assets/everyday_image.jpg",
+      "logo": "images/team_assets/everyday_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Tray",
       "url": "https://www.tray.io",
-      "tags": "playground fund"
+      "tidyurl": "tray.io",
+      "tags": "playground fund",
+      "image": "images/team_assets/tray_image.jpg",
+      "logo": "images/team_assets/tray_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Uptree",
       "url": "https://www.uptree.co",
-      "tags": "playground fund"
+      "tidyurl": "uptree.co",
+      "tags": "playground fund",
+      "image": "images/team_assets/uptree_image.jpg",
+      "logo": "images/team_assets/uptree_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Dojo",
       "url": "https://www.dojoapp.co",
-      "tags": "fund"
+      "tidyurl": "dojoapp.co",
+      "tags": "fund",
+      "image": "images/team_assets/dojo_image.jpg",
+      "logo": "images/team_assets/dojo_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Hoop",
       "url": "https://www.hoop.co.uk",
-      "tags": "fund"
+      "tidyurl": "hoop.co.uk",
+      "tags": "fund",
+      "image": "images/team_assets/hoop_image.jpg",
+      "logo": "images/team_assets/hoop_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Wayfindr",
       "url": "https://www.wayfindr.net",
-      "tags": "venture"
+      "tidyurl": "wayfindr.net",
+      "tags": "venture",
+      "image": "images/team_assets/wayfindr_image.jpg",
+      "logo": "images/team_assets/wayfindr_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Everpress",
       "url": "https://www.everpress.com",
-      "tags": "fund"
+      "tidyurl": "everpress.com",
+      "tags": "fund",
+      "image": "images/team_assets/everpress_image.jpg",
+      "logo": "images/team_assets/everpress_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Curiscope",
       "url": "https://www.curiscope.com",
-      "tags": "fund"
+      "tidyurl": "curiscope.com",
+      "tags": "fund",
+      "image": "images/team_assets/curiscope_image.jpg",
+      "logo": "images/team_assets/curiscope_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Amaliah",
       "url": "https://www.amaliah.com",
-      "tags": "fund"
+      "tidyurl": "amaliah.com",
+      "tags": "fund",
+      "image": "images/team_assets/amaliah_image.jpg",
+      "logo": "images/team_assets/amaliah_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Mayku",
       "url": "https://www.mayku.me",
-      "tags": "fund"
+      "tidyurl": "mayku.me",
+      "tags": "fund",
+      "image": "images/team_assets/mayku_image.jpg",
+      "logo": "images/team_assets/mayku_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Marvel",
       "url": "https://www.marvelapp.com",
-      "tags": "fund"
+      "tidyurl": "marvelapp.com",
+      "tags": "fund",
+      "image": "images/team_assets/marvel_image.jpg",
+      "logo": "images/team_assets/marvel_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Dice",
       "url": "https://www.dice.fm",
-      "tags": "fund venture"
+      "tidyurl": "dice.fm",
+      "tags": "fund venture",
+      "image": "images/team_assets/dice_image.jpg",
+      "logo": "images/team_assets/dice_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "ustwo Games",
       "url": "https://www.ustwogames.co.uk",
-      "tags": "venture"
+      "tidyurl": "ustwogames.co.uk",
+      "tags": "venture",
+      "image": "images/team_assets/ustwogames_image.jpg",
+      "logo": "images/team_assets/ustwogames_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Pilcro",
       "url": "https://www.pilcro.com",
-      "tags": "playground"
+      "tidyurl": "pilcro.com",
+      "tags": "playground",
+      "image": "images/team_assets/pilcro_image.jpg",
+      "logo": "images/team_assets/pilcro_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Creative Mentor Network",
       "url": "http://creativementornetwork.org/",
-      "tags": "playground"
+      "tidyurl": "creativementornetwork.org",
+      "tags": "playground",
+      "image": "images/team_assets/creativementornetwork_image.jpg",
+      "logo": "images/team_assets/creativementornetwork_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Pauseable",
       "url": "http://www.pauseable.com",
-      "tags": "fund"
+      "tidyurl": "pauseable.com",
+      "tags": "fund",
+      "image": "images/team_assets/pauseable_image.jpg",
+      "logo": "images/team_assets/pauseable_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Over",
       "url": "https://www.madewithover.com",
-      "tags": "fund"
+      "tidyurl": "madewithover.com",
+      "tags": "fund",
+      "image": "images/team_assets/over_image.jpg",
+      "logo": "images/team_assets/over_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Run an Empire",
       "url": "http://www.runanempire.com",
-      "tags": "fund"
+      "tidyurl": "runanempire.com",
+      "tags": "fund",
+      "image": "images/team_assets/runanempire_image.jpg",
+      "logo": "images/team_assets/runanempire_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Brother Cycles",
       "url": "https://www.brothercycles.com/",
-      "tags": "fund"
+      "tidyurl": "brothercycles.com",
+      "tags": "fund",
+      "image": "images/team_assets/brothercycles_image.jpg",
+      "logo": "images/team_assets/brothercycles_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Paul James",
       "url": "http://pauljamess.com/",
-      "tags": ""
+      "tidyurl": "pauljamess.com",
+      "tags": "",
+      "image": "images/team_assets/pauljames_image.jpg",
+      "logo": "images/team_assets/pauljames_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Gather Data",
       "url": "https://gatherdata.co/",
-      "tags": ""
+      "tidyurl": "gatherdata.co",
+      "tags": "",
+      "image": "images/team_assets/gather_image.jpg",
+      "logo": "images/team_assets/gather_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "OH",
       "url": "https://ohthisway.co.uk/",
-      "tags": "foundation"
+      "tidyurl": "ohthisway.co.uk",
+      "tags": "foundation",
+      "image": "images/team_assets/oh_image.jpg",
+      "logo": "images/team_assets/oh_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Studio Something",
       "url": "http://studiosomething.co/",
-      "tags": ""
+      "tidyurl": "studiosomething.co",
+      "tags": "",
+      "image": "images/team_assets/studiosomething_image.jpg",
+      "logo": "images/team_assets/studiosomething_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Raise Your Hands",
       "url": "https://www.raiseyourhands.org.uk/",
-      "tags": "playground"
+      "tidyurl": "raiseyourhands.org.uk",
+      "tags": "playground",
+      "image": "images/team_assets/raiseyourhands_image.jpg",
+      "logo": "images/team_assets/raiseyourhands_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Grae Matta Foundation",
       "url": "http://graematta.com/",
-      "tags": "playground"
+      "tidyurl": "graematta.com",
+      "tags": "playground",
+      "image": "images/team_assets/graematta_image.jpg",
+      "logo": "images/team_assets/graematta_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "No Mayo",
       "url": "http://www.no-mayo.digital/",
-      "tags": "playground"
+      "tidyurl": "no-mayo.digital",
+      "tags": "playground",
+      "image": "images/team_assets/nomayo_image.jpg",
+      "logo": "images/team_assets/nomayo_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Morrama",
       "url": "http://www.morrama.com/",
-      "tags": "playground"
+      "tidyurl": "morrama.com",
+      "tags": "playground",
+      "image": "images/team_assets/morrama_image.jpg",
+      "logo": "images/team_assets/morrama_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Mr Bingo",
       "url": "http://mr.bingo/",
-      "tags": "playground"
+      "tidyurl": "mr.bingo",
+      "tags": "playground",
+      "image": "images/team_assets/mrbingo_image.jpg",
+      "logo": "images/team_assets/mrbingo_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Seed and Spark",
       "url": "https://www.seedandspark.com/",
-      "tags": "fund"
+      "tidyurl": "seedandspark.com",
+      "tags": "fund",
+      "image": "images/team_assets/seedandspark_image.jpg",
+      "logo": "images/team_assets/seedandspark_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Loop",
       "url": "http://loopsocial.co/",
-      "tags": "playground"
+      "tidyurl": "loopsocial.co",
+      "tags": "playground",
+      "image": "images/team_assets/loop_image.jpg",
+      "logo": "images/team_assets/loop_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Lecture in Progress",
       "url": "https://lectureinprogress.com/",
-      "tags": "foundation"
+      "tidyurl": "lectureinprogress.com",
+      "tags": "foundation",
+      "image": "images/team_assets/lectureinprogress_image.jpg",
+      "logo": "images/team_assets/lectureinprogress_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "UNRD",
       "url": "http://unrd.co/",
-      "tags": "playground"
+      "tidyurl": "unrd.co",
+      "tags": "playground",
+      "image": "images/team_assets/unrd_image.jpg",
+      "logo": "images/team_assets/unrd_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Curate Labs",
       "url": "https://curatelabs.co/",
-      "tags": "playground"
+      "tidyurl": "curatelabs.co",
+      "tags": "playground",
+      "image": "images/team_assets/curatelabs_image.jpg",
+      "logo": "images/team_assets/curatelabs_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Remote Zoo",
       "url": "https://www.remotezoo.com/",
-      "tags": "playground"
+      "tidyurl": "remotezoo.com",
+      "tags": "playground",
+      "image": "images/team_assets/remotezoo_image.jpg",
+      "logo": "images/team_assets/remotezoo_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Tom Gaul",
       "url": "http://www.tomgaul.com/",
-      "tags": "playground"
+      "tidyurl": "tomgaul.com",
+      "tags": "playground",
+      "image": "images/team_assets/tomgaul_image.jpg",
+      "logo": "images/team_assets/tomgaul_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Design Club",
       "url": "http://designclub.org.uk/",
-      "tags": "foundation"
+      "tidyurl": "designclub.org.uk",
+      "tags": "foundation",
+      "image": "images/team_assets/designclub_image.jpg",
+      "logo": "images/team_assets/designclub_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Suvera",
       "url": "http://suvera.co.uk/",
-      "tags": ""
+      "tidyurl": "suvera.co.uk",
+      "tags": "",
+      "image": "images/team_assets/suvera_image.jpg",
+      "logo": "images/team_assets/suvera_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Kyra",
       "url": "https://kyra.com/",
-      "tags": "fund"
+      "tidyurl": "kyra.com",
+      "tags": "fund",
+      "image": "images/team_assets/kyra_image.jpg",
+      "logo": "images/team_assets/kyra_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Onlicar",
       "url": "https://www.onlicar.com/",
-      "tags": "playground"
+      "tidyurl": "onlicar.com",
+      "tags": "playground",
+      "image": "images/team_assets/onlicar_image.jpg",
+      "logo": "images/team_assets/onlicar_logo.jpg",
+      "line": "",
+      "copy": ""
     },
     {
       "name": "Alix Fox",
-      "url": "https://twitter.com/AlixFox",
-      "tags": "playground"
+      "url": "https://twitter.com/AlixFox/",
+      "tidyurl": "twitter.com/AlixFox",
+      "tags": "playground",
+      "image": "images/team_assets/alixfox_image.jpg",
+      "logo": "images/team_assets/alixfox_logo.jpg",
+      "line": "",
+      "copy": ""
     }
   ];
 
