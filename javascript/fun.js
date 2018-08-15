2cfb5092d8b57338d3976b10f38fc359fcf4f30b
Neef Rehman <neef.rehman@gmail.com>
2018-08-15 16:12:09 +0100

    :)

--- javascript/fun.js
+++ javascript/fun.js
@@ -5,7 +5,7 @@ $document.mousemove(function(e) {
     var width = $document.width() / 255;
     var pageX = e.pageX / width;
     var pageY = e.pageY / width;
-    var valueX = 255 - pageX;
+    var valueX = pageX;
     var valueY = 255 - pageY;
 
     $("span.stretch").css("transform", "scale(" + valueX/102 + "," + valueY/100 +")");
