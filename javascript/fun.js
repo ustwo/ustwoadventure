f4a4d6ee94911c4ae7a384c1534cebd77311be5f
Neef Rehman <neef.rehman@gmail.com>
2018-08-17 17:29:20 +0100

    :)

--- javascript/fun.js
+++ javascript/fun.js
@@ -5,10 +5,20 @@ $document.mousemove(function(e) {
     var width = $document.width() / 255;
     var pageX = e.pageX / width;
     var pageY = e.pageY / width;
+    var valueX = pageX + 200;
+    var valueY = 255 - pageY;
+
+    $("span.stretch").css("transform", "scale(" + valueX/200 + "," + valueY/50 +")");
+});
+
+$document.mousemove(function(e) {
+    var width = $document.width() / 255;
+    var pageX = e.pageX / width;
+    var pageY = e.pageY / width;
     var valueX = pageX - 122.5;
     var valueY = 122.5 - pageY;
 
-    $("span.stretch").css("transform", "scale(" + valueX/50 + "," + valueY/25 +")");
+    $("span.stretch.clear").css("transform", "scale(" + valueX/50 + "," + valueY/25 +")");
 });
 
 
@@ -17,5 +27,8 @@ window.ondevicemotion = function(event) {
   var y = event.accelerationIncludingGravity.y;
   var z = event.accelerationIncludingGravity.z;
 
-  $("span.stretch").css("transform", "scale(" + (x+3)/5 + "," + ((-y)+3)/5 +")");
+  $("span.stretch").css("transform", "scale(" + x/5 + "," + y/5 +")");
+
+  $("span.stretch.clear").css("transform", "scale(" + (x+3)/5 + "," + ((-y)+3)/5 +")");
+
 }
