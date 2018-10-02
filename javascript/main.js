fd97d57eb5073640ae4d5e649806506b5e6ffdb4
Neef Rehman <neef.rehman@gmail.com>
2018-10-02 10:09:15 +0100

    :)

--- javascript/main.js
+++ javascript/main.js
@@ -631,16 +631,16 @@ const portfolioArray = [
     "line": "Humanising Computing",
     "copy": "We've created an SDK for emotional intelligence. Using AI models we want to enable products that interact with users as naturally as the interactions between two humans."
   },
-  // {
-  //   "name": "Creative Hustle",
-  //   "url": "https://creativehustle.org",
-  //   "tidyurl": "creativehustle.org",
-  //   "tags": "foundation",
-  //   "image": "images/team_assets/creativehustle_image.jpg",
-  //   "logo": "images/team_assets/creativehustle_logo.png",
-  //   "line": "A launchpad for creatives & techies of colour",
-  //   "copy": "We are a non-profit organisation dedicated to help young creatives and techies of colour grow. Through awarding micro-grants, we aim to plant seeds to help young creative and techies progress further in their craft or discipline."
-  // },
+  {
+    "name": "Creative Hustle",
+    "url": "https://creativehustle.org",
+    "tidyurl": "creativehustle.org",
+    "tags": "foundation",
+    "image": "images/team_assets/creativehustle_image.jpg",
+    "logo": "images/team_assets/creativehustle_logo.png",
+    "line": "A launchpad for creatives & techies of colour",
+    "copy": "We are a non-profit organisation dedicated to help young creatives and techies of colour grow. Through awarding micro-grants, we aim to plant seeds to help young creative and techies progress further in their craft or discipline."
+  },
   {
     "name": "Bounce Cinema",
     "url": "https://bouncecinema.com",
