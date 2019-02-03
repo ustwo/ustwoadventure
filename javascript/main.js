650465834cfc353f5d94f20d2beb09bcea4ab0eb
Neef Rehman <neef.rehman@gmail.com>
2019-02-03 11:52:13 +0000

    9s -> 8s

--- javascript/main.js
+++ javascript/main.js
@@ -264,8 +264,8 @@ const logoUstwo = document.querySelector("header .logo img");
 window.addEventListener("deviceorientation", e => {
     const tilt = (window.innerHeight > window.innerWidth) ? e.gamma / 2 : e.beta;
 
-    logoAdventure.style.transition = "transform 9s cubic-bezier(0.6, 0, 0.5, 1)";
-    logoUstwo.style.transition = "transform 9s cubic-bezier(0.6, 0, 0.5, 1)";
+    logoAdventure.style.transition = "transform 8s cubic-bezier(0.6, 0, 0.5, 1)";
+    logoUstwo.style.transition = "transform 8s cubic-bezier(0.6, 0, 0.5, 1)";
     logoAdventure.style.transform = `rotate(${-(tilt + 10)}deg)`;
     logoUstwo.style.transform = `rotate(${(tilt + 10)}deg)`;
 }, true);
