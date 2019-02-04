3fda064232eb358dc5af8fb26714f146d326ea98
Neef Rehman <neef.rehman@gmail.com>
2019-02-04 17:45:14 +0000

    3s -> 4s

--- javascript/main.js
+++ javascript/main.js
@@ -307,8 +307,8 @@ window.ondeviceorientation = e => {
     const tilt = (window.innerHeight > window.innerWidth) ? e.gamma / 2 : e.beta;
     const cappedTilt = Math.max(-45, Math.min(20, tilt));
 
-    logoAdventure.style.transition = "transform 3s cubic-bezier(0.6, 0, 0.5, 1)";
-    logoUstwo.style.transition = "transform 3s cubic-bezier(0.6, 0, 0.5, 1)";
+    logoAdventure.style.transition = "transform 4s cubic-bezier(0.6, 0, 0.5, 1)";
+    logoUstwo.style.transition = "transform 4s cubic-bezier(0.6, 0, 0.5, 1)";
     logoAdventure.style.transform = `rotate(${-(cappedTilt + 10)}deg)`;
     logoUstwo.style.transform = `rotate(${(cappedTilt + 10)}deg)`;
 };
