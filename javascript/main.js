a14bb94515bed883964c036973a74730c6387251
Neef Rehman <neef.rehman@gmail.com>
2019-02-07 11:44:14 +0000

    remove cap

--- javascript/main.js
+++ javascript/main.js
@@ -314,17 +314,17 @@ lzy();
 
 
 
-const logoAdventure = document.querySelector("header .logo");
+const logoRing = document.querySelector("header .logo");
 const logoUstwo = document.querySelector("header .logo img");
 
 window.ondeviceorientation = e => {
-    const tilt = (window.innerHeight > window.innerWidth) ? e.gamma / 2 : e.beta;
-    const cappedTilt = Math.max(-45, Math.min(20, tilt));
+    const tilt = (window.innerHeight > window.innerWidth) ? e.gamma : e.beta;
+    // const cappedTilt = Math.max(-45, Math.min(20, tilt));
 
-    logoAdventure.style.transition = "transform 6s cubic-bezier(0.6, 0, 0.5, 1)";
-    logoUstwo.style.transition = "transform 6s cubic-bezier(0.6, 0, 0.5, 1)";
-    logoAdventure.style.transform = `rotate(${-(cappedTilt + 10)}deg)`;
-    logoUstwo.style.transform = `rotate(${(cappedTilt + 10)}deg)`;
+    logoRing.style.transition = "transform 5s cubic-bezier(0.6, 0, 0.5, 1)";
+    logoUstwo.style.transition = "transform 5s cubic-bezier(0.6, 0, 0.5, 1)";
+    logoRing.style.transform = `rotate(${-(tilt + 10)}deg)`;
+    logoUstwo.style.transform = `rotate(${(tilt + 10)}deg)`;
 };
 
 
