4381fd4b35d825c2c6b3c21fc73bbd9230a269b2
Neef Rehman <neef.rehman@gmail.com>
2019-02-27 15:31:49 +0000

    arrow data-src

--- javascript/0_portfolioArray.js
+++ javascript/0_portfolioArray.js
@@ -233,7 +233,7 @@ const createPortfolioItems = a => a.map(obj => {
             <p class="name">${ obj.name }</p>
             <div class="image-container">
                 <img class="company-image" data-src="${ obj.image }" alt="${ obj.name }'s company photo">
-                <img class="arrow" src="images/arrow_white.svg" alt="">
+                <img class="arrow" data-src="images/arrow_white.svg" alt="">
             </div>
             <p class="copy">${ obj.line }</p>
         </a>
