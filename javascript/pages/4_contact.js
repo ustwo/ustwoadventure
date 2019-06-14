72aae43f8797c91e46866465d81de3cf9fa4f3ca
Neef Rehman <neef.rehman@gmail.com>
2019-06-14 15:26:26 +0100

    changes

--- javascript/pages/4_contact.js
+++ javascript/pages/4_contact.js
@@ -27,7 +27,7 @@ contactPage.innerHTML = `
 
         </div>
 
-        <form class="form" name="contact" method="POST" netlify netlify-honeypot="bot-field">
+        <form class="form" name="contact" method="POST" action="contact?thanks" netlify netlify-honeypot="bot-field">
 
             <p class="hidden">
                 <label> Don’t fill this out: <input name="bot-field"> </label>
