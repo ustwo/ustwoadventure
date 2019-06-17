6d07c7cdb26e026b825d1f292918b20a9099a88d
Neef Rehman <neef.rehman@gmail.com>
2019-06-17 12:10:33 +0100

    AJAX form send

--- javascript/pages/4_contact.js
+++ javascript/pages/4_contact.js
@@ -1,3 +1,55 @@
+const formSubmit = () => {
+    const form = document.querySelector("form");
+    const h1 = document.querySelector("h1");
+    const copy = document.querySelector(".copy p.message");
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+
+        const data = {};
+        for (let i = 0; i < form.length; ++i) {
+            const input = form[i];
+            if (input.name) {
+                data[input.name] = input.value;
+            }
+        }
+
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action, true);
+        xhr.setRequestHeader("Accept", "application/json; charset=utf-8");
+        xhr.setRequestHeader("Content-Type", "application/json; charset=UTF-8");
+        xhr.send(JSON.stringify(data));
+
+        xhr.onloadend = response => {
+            if (response.target.status === 0) {
+                h1.textContent = "Error";
+                copy.innerHTML = form.dataset.formError;
+            } else if (response.target.status === 400) {
+                h1.textContent = "Error";
+                copy.innerHTML = JSON.parse(responseText).error;
+            } else if (response.target.status === 200) {
+                h1.textContent = "Thanks for getting in touch";
+                copy.textContent = `We'll read through what you've sent us and follow up via
+                                    e-mail as soon as possible. Have a great day!`;
+
+                for (let i = 0; i < form.length; ++i) {
+                    const input = form[i];
+                    input.disabled = true;
+                }
+            }
+
+            window.scrollTo({
+                top: 0,
+                left: 0,
+                behavior: "smooth"
+            });
+        };
+    });
+};
+
+
+
+
 const contactPage = document.createElement("div");
 contactPage.className = "contact-content";
 
@@ -9,7 +61,7 @@ contactPage.innerHTML = `
 
         <div class="copy">
 
-            <p>
+            <p class="message">
                 To get in touch with us to talk about an investment opportunity, please
                 fill out the form. To contact us about anything else, please send us
                 an email to the address below.
@@ -21,7 +73,7 @@ contactPage.innerHTML = `
 
         </div>
 
-        <form name="investment-contact" action="contact-thanks" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+        <form name="investment-contact" action="/contact-thanks" method="POST" data-netlify="true" netlify-honeypot="bot-field">
             
             <input type="hidden" name="form-name" value="investment-contact"/>
             <label class="hidden"> Don’t fill this out: <input name="bot-field"> </label>
