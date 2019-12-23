6e3a5534009d6eee4f22aec85bf736e62487000f
Neef Rehman <neef.rehman@gmail.com>
2019-12-23 12:49:37 +0000

    404 styles

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -9,18 +9,26 @@ import SectionWrapper from "../components/section-wrapper";
 const StyledMain = styled(MainWrapper)`
     h1 {
         margin-bottom: 80px;
-        margin-right: 10%;
         margin-left: auto;
         width: max-content;
     }
 
     a {
-        margin-left: 10%;
         margin-bottom: 50px;
+        width: 130px;
+    }
+
+    @media (max-width: 500px) {
+        h1 {
+            margin-right: 10%;
+        }
+
+        a {
+            margin-left: 10%;
+            width: 120px;
+        }
     }
 `;
-
-const Shruggie = styled.h1``;
 
 const FourOhFour = () => (
     <StyledMain>
@@ -29,7 +37,7 @@ const FourOhFour = () => (
         <SectionWrapper style={{ display: "block" }}>
             <h1>Page not found</h1>
 
-            <Shruggie>¯\\_(ツ)_/¯</Shruggie>
+            <h1>¯\_(ツ)_/¯</h1>
 
             <Button back href="/">
                 Go home
