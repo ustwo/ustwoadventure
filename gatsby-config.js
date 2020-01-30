7c4470997be0a51b4a0d799de892f8e0c42452d7
Neef Rehman <neef.rehman@gmail.com>
2020-01-30 12:35:23 +0000

    meta tag changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
         siteUrl: "https://adventure.ustwo.com",
         siteTitle: "ustwo Adventure",
         siteDescription:
-            "Investing in creative companies, Differently — We help founders build businesses on top of strong culture, values and design.",
+            "Investing in creative companies, differently — We work with and support founders who are building businesses on top of strong culture, values and design.",
         siteImage: "https://adventure.ustwo.com/meta_image.png",
         twitter: "@ustwoadventure",
         name: "ustwo Adventure"
