115a4cd216251709cba7262b74a0185b4a094994
phil-linnell <phil@ustwo.com>
2025-06-30 15:20:34 +0100

    Fix

--- src/hooks/use-site-metadata.js
+++ src/hooks/use-site-metadata.js
@@ -1,22 +1,11 @@
-import { useStaticQuery, graphql } from "gatsby";
-
+// Simple site metadata hook without GraphQL to avoid build issues
 export default () => {
-    const { site } = useStaticQuery(
-        graphql`
-            query SiteMetaData {
-                site {
-                    siteMetadata {
-                        siteUrl
-                        siteTitle
-                        siteDescription
-                        siteImage
-                        twitter
-                        name
-                    }
-                }
-            }
-        `
-    );
-
-    return site.siteMetadata;
+    return {
+        siteUrl: "https://adventure.ustwo.com",
+        siteTitle: "ustwo Adventure",
+        siteDescription: "We created ustwo Adventures to support early-stage initiatives grounded in creativity and the desire to make a positive impact.",
+        siteImage: "https://adventure.ustwo.com/meta_image.png",
+        twitter: "@ustwoadventure",
+        name: "ustwo Adventure"
+    };
 };
